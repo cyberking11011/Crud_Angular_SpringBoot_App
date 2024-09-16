83f8c177d76863f8926cbbd212dcc45a2b951151
Sehriyar REHIMOV <sehriyar.rehimov@akgun.com.tr>
2024-09-16 17:41:11 +0400

    excel fix

--- src/app/componenets/user/user.component.ts
+++ src/app/componenets/user/user.component.ts
@@ -5,14 +5,12 @@ import { Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import {
-  MatDialog,
-} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { DeleteUserComponent } from '../delete-user/delete-user.component';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
-import { style } from '@angular/animations';
-
+import { ExportExcelService } from 'src/app/services/export-excel.service';
+import { saveAs } from '@progress/kendo-file-saver';
 
 @Component({
   selector: 'app-user',
@@ -30,6 +28,8 @@ export class UserComponent implements OnInit {
   };
 
   searchValue: string = '';
+  path: string = 'C:/Users/user/Desktop';
+  fileName:string='';
 
   displayedColumns: string[] = [
     'id',
@@ -47,11 +47,9 @@ export class UserComponent implements OnInit {
     private window: Window,
     private userService: UserService,
     private router: Router,
-    private deleteDialog: MatDialog
-  ) {
-   
-    
-  }
+    private deleteDialog: MatDialog,
+    private excelService: ExportExcelService
+  ) {}
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
@@ -103,15 +101,23 @@ export class UserComponent implements OnInit {
     this.window.print();
   }
   pdfGenerator() {
-    
     let pdf = new jsPDF('p', 'pt', 'a4');
 
-    pdf.setFont('Roboto-Regular','normal');
-    
-    autoTable(pdf, { html: '#user-table' });
-   
-    pdf.save('home.pdf');
-    
+    pdf.setFont('Roboto-Regular', 'normal');
 
+    autoTable(pdf, { html: '#user-table' });
+
+    pdf.save('home.pdf');
+  }
+
+  excelGenerator(search:string,path:string,fileName:string) {
+    
+    this.excelService
+      .exportExcel(this.searchValue, this.path,this.fileName)
+      .subscribe((excel) => {
+        
+
+       
+      });
   }
 }
--- src/app/services/export-excel.service.ts
+++ src/app/services/export-excel.service.ts
@@ -1,24 +1,37 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserComponent } from '../componenets/user/user.component';
+import { WorkBook } from 'xlsx';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExportExcelService {
+  
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
+  headers = ["ID", "Name", "Surname", "Email", "Salary"];
 
-   headers=[
-    "ID",
-    "Name",
-    "Surname",
-    "Email",
-    "Salary"
+  exportExcel(search:string,path:string,fileName:string):Observable<any> {
+    // const headers=new HttpHeaders()
+    // .set('ID','ID')
+    // .set('Name','Name')
+    // .set('Surname','Surname')
+    // .set('Email','Email')
+    // .set('Salary','Salary');
 
-  ];
+    const params = new HttpParams()
+      .set('search', search)
+      .set('path', path)
+      .set("file_name",fileName);
 
-  exportExcel(){
-    
+   return this.http.post(
+      'http://localhost:8080/api/v1/users/export-excel',
+      this.headers,
+      {params:params},
+     
+      
+    );
   }
-
 }
